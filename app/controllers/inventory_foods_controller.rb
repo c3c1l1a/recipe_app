79dab8834575442df4a63c70d2905086f06b1266
petushka1 <nadya@devery.io>
2022-10-25 23:50:22 +0300

    setup inventory_food

---
+++ app/controllers/inventory_foods_controller.rb
@@ -0,0 +1,28 @@
+class InventoryFoodsController < ApplicationController
+    def new
+        @inventory_food = InventoryFood.new
+        @inventory, = Inventory.where(id: params[:inventory_id], user: current_user)
+    end
+
+    def create
+        @inventory, = Inventory.where(id: params[:inventory_id], user: current_user)
+        @inventory_food = InventoryFood.new(create_params) 
+        if @inventory_food.save
+            redirect_to(inventory_path(@inventory))
+        else
+            render(:new, alert: 'Nothing was Added')
+        end
+    end
+
+    def destroy
+        @inventory_food, = RecipeFood.where(params.permit(:id, :inventory_id))
+        @inventory_food.destroy
+        redirect_to(inventory_path(params[:inventory_id]))
+    end
+
+    private
+
+    def create_params
+        params.permit(:quantity, inventory_id)
+    end
+end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -65,6 +65,12 @@ class RecipesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+    params.require(:recipe)
+    .permit(:name, 
+      :preparation_time, 
+      :cooking_time, 
+      :description, 
+      :public, 
+      :user_id)
   end
 end
